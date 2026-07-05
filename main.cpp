07003dd60865b1de4fdbdbdadb58c4dbda6602f5
tarabayhiba <tarabayhiba05@gmail.com>
2026-07-06 00:46:03 +0300

    feat: add menu loop skeleton

--- main.cpp
+++ main.cpp
@@ -53,8 +53,35 @@ std::string read_nonempty_string(const std::string& prompt) {
     }
 }
 
+void print_menu() {
+    std::cout << "\n=== Robot Fleet Manager ===\n"
+               << "1. Add robot\n"
+               << "2. Remove robot\n"
+               << "3. Show all robots\n"
+               << "4. Work (single robot)\n"
+               << "5. Work all\n"
+               << "6. Charge all\n"
+               << "7. Assign task to robot\n"
+               << "8. Show task queue\n"
+               << "9. Start timed work on a robot\n"
+               << "0. Exit\n";
+}
+
 }  // namespace
 
 int main() {
+    Fleet fleet;
+
+    while (true) {
+        print_menu();
+        int choice = read_int("Choice: ");
+
+        if (choice == 0) {
+            break;
+        }
+
+        std::cout << "Option not implemented yet.\n";
+    }
+
     return 0;
 }
